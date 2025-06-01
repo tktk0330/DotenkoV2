f49eb6acc235c46a1195bf6c67e392ea795d2043
tktk0330 <tkmsnd0330@gmail.com>
2025-06-01 10:43:48 +0900

    Update PlayerModel.swift

--- DotenkoV2/Model/Game/PlayerModel.swift
+++ DotenkoV2/Model/Game/PlayerModel.swift
@@ -1,6 +1,16 @@
 import SwiftUI
 
-// プレイヤーモデル
+/// プレイヤーモデル
+/// - Parameters:
+///   - id: ユーザーID
+///   - side: ユーザーサイド
+///   - name: ユーザー名
+///   - iconUrl: プロフィール画像のURL（オプション）
+///   - hand: 手札
+///   - selectedCards: 選択中カード
+///   - score: ゲームスコア
+///   - rank: 順位
+///   - dtnk: どてんこ可能フラグ
 struct Player: Identifiable {
     let id: String
     let side: Int
